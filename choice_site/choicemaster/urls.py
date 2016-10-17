bf6755d2d41629571ba4554a007caa56ac093661
Esteban Boucher <eboucher7@gmail.com>
2016-10-17 15:59:16 -0300

    Vista de index desde accounts/profile agregada para que no tire error luego de loguearse.

--- choice_site/choicemaster/urls.py
+++ choice_site/choicemaster/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
 from . import views
+from django.contrib import admin
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^index$', views.index, name='index'),
+    url(r'^accounts/profile', views.index, name='index'),
 ]
