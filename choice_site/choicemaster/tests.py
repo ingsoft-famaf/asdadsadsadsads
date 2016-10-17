9eae71bd16bbca95a763b0daf29213460ee09625
Esteban Boucher <eboucher7@gmail.com>
2016-10-17 20:05:17 -0300

    Documentación de tests agregada.

--- choice_site/choicemaster/tests.py
+++ choice_site/choicemaster/tests.py
@@ -5,7 +5,9 @@ from django.contrib.auth.models import User
 class UserTestCase(TestCase):
 
     def setUp(self):
-        """A new user is created and verifies that login function properly"""
+        """
+        Create a new user and verify that the login function works properly
+        """
         self.user = User.objects.create(username='testuser')
         self.user.set_password('12345')
         self.user.save()
@@ -13,23 +15,43 @@ class UserTestCase(TestCase):
         self.logged_in = self.c.login(username='testuser', password='12345')
 
     def test_user_can_login(self):
+        """
+        Check that the user entity has its logged_in state equal to TRUE,
+        since it just logged in
+        """
         self.assertTrue(self.logged_in)
 
     def test_login_view(self):
+        """
+        Logout from an account, check that the login view is displayed
+        successfully, and login with a username and a password
+        """
         self.c.logout()
         response = self.c.get('/accounts/login/')
         self.assertEqual(response.status_code, 200)
         self.logged_in = self.c.login(username='testuser', password='12345')
 
     def test_index_view(self):
+        """
+        Make sure that our homepage returns an HTTP 200 status code for a
+        successful HTTP request
+        """
         response = self.c.get('/')
         self.assertEqual(response.status_code, 200)
 
     def test_logout_view(self):
+        """
+        Test that our website returns an HTTP 200 status after a successful
+        logout request 
+        """
         response = self.c.get('/accounts/logout/')
         self.assertEqual(response.status_code, 200)
 
     def test_signup_view(self):
+        """
+        Check that our website returns an HTTP 200 status after a successful
+        signup request
+        """
         self.c.logout()
         response = self.c.get('/accounts/signup/')
         self.assertEqual(response.status_code, 200)
