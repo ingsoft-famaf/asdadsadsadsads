39b6e1173574f31b0b7a195004462c7caf2ab67f
Esteban Boucher <eboucher7@gmail.com>
2016-10-24 11:54:30 -0300

    Agregado método para parsear archivos XML usando 'etree' de la librería lxml.

--- choice_site/choicemaster/views.py
+++ choice_site/choicemaster/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from lxml import etree
 
 
 @login_required
 def index(request):
     return render(request, 'choicemaster/index.html')
+
+tree = etree.parse("/choicemaster/questionSample.xml")
+
+for user in tree.xpath("/choicemaster/questionSample.xml"):
+    print(user.text)
