748cda479572cd96ad75a4d174258ffbdb3f653d
Agustin Urquiza <agustin.h.urquiza@gmail.com>
2016-10-24 00:02:58 -0300

    No se cuenta mas los reportes que ya fueron evaluados

--- choice_site/choicemaster/views.py
+++ choice_site/choicemaster/views.py
@@ -4,5 +4,10 @@ from models import Report
 
 @login_required
 def index(request):
-    return render(request, 'choicemaster/index.html',{"reported":
-                                                          Report.objects.count()})
+    return render(request, 'choicemaster/index.html',
+                  {"reported": Report.objects.exclude(report_state='E')
+                  .count()})
+"""
+Le paso al template la cantidad de reportes sin ser evaluados que hay en el
+momento.
+"""
