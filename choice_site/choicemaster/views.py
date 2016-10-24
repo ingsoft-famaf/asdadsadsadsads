a7917c77639b94497297a38fe29522e85d3b6cdd
Agustin Urquiza <agustin.h.urquiza@gmail.com>
2016-10-23 21:44:08 -0300

    Agrege el menu de denuncias y su cantidad

--- choice_site/choicemaster/views.py
+++ choice_site/choicemaster/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-
+from models import Report
 
 @login_required
 def index(request):
-    return render(request, 'choicemaster/index.html')
+    return render(request, 'choicemaster/index.html',{"reported":
+                                                          Report.objects.count()})
