9ebddc4ed580b367fb86be6503f952fca0db2bd4
Esteban Boucher <eboucher7@gmail.com>
2016-10-24 14:17:57 -0300

    Modificada función de parseo y archivo xml de preguntas.

--- choice_site/choicemaster/views.py
+++ choice_site/choicemaster/views.py
@@ -1,13 +1,28 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from lxml import etree
+from StringIO import StringIO
 
 
 @login_required
 def index(request):
     return render(request, 'choicemaster/index.html')
 
-tree = etree.parse("/choicemaster/questionSample.xml")
 
-for user in tree.xpath("/choicemaster/questionSample.xml"):
-    print(user.text)
+def parseXML(xmlFile):
+    """
+    Parse the xml
+    """
+    f = open(xmlFile)
+    xml = f.read()
+    f.close()
+ 
+    tree = etree.parse(StringIO(xml))
+    # e.g. tree = etree.parse("/choicemaster/questionSample.xml")
+    context = etree.iterparse(StringIO(xml))
+    for action, elem in context:
+        if not elem.text:
+            text = "None"
+        else:
+            text = elem.text
+        print elem.tag + " => " + text
