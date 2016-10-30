3134f12373bf77c49b4e18064fbc10ab3a877167
Esteban Boucher <eboucher7@gmail.com>
2016-10-30 18:32:50 -0300

    Pruebas de distintas formas de subir un archivo para los tests de cargar preguntas

--- choice_site/choicemaster/views.py
+++ choice_site/choicemaster/views.py
@@ -5,11 +5,18 @@ from choicemaster import models
 from .forms import UploadFileForm
 
 from .upload import parse_xml_question
+from .models import Report
 
 
 @login_required
-def index(request):
-    return render(request, 'choicemaster/index.html')
+def index(request, message=''):
+    if message:
+        context = {'message': message}
+    else:
+        context = {'message': 'Everything ok!'}
+    context['reported'] = Report.objects.exclude(report_state='E').count()
+
+    return render(request, 'choicemaster/index.html', context)
 
 
 @login_required
@@ -33,24 +40,46 @@ def add_question_w_subject(request, subject_id):
 
 @login_required
 @staff_member_required
-def add_question_w_subject_topic(request, subject_id, topic_id):
+def add_question_w_subject_topic(request, subject_id, topic_id, message=''):
     subject = models.Subject.objects.filter(id=subject_id)[0]
     topic = models.Topic.objects.filter(id=topic_id)[0]
     context = dict()
     context['subject'] = subject
     context['topic'] = topic
+    if message:
+        context['message'] = message
+    else:
+        context['message'] = 'Everything ok!'
 
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
             result = parse_xml_question(request.FILES['docfile'], topic_id)
-            if result['status'] == True:
-                return redirect(index)
+            # TODO revisar como hacer el redirect.
+            
+            if result['status']:
+                context['message'] = result['message']
+                return render(request, 'choicemaster/index.html',context)
             else:
-                return HttpResponse(result['message'])
+                context['message'] = result['message']
+                return render(request, 'choicemaster/add/question/w_subject_topic.html',context)
+
+            
+            # return redirect('index')
     else:
         form = UploadFileForm()
         context['form'] = form
 
     return render(request, 'choicemaster/add/question/w_subject_topic.html',
         context)
+
+def report(request):
+    # TODO Check function. Documentation doesn't match implementation. Spanish?
+    """
+    Le paso al template la cantidad de reportes sin ser evaluados que hay en el
+    momento.
+    """
+    context = dict()
+    context['reports'] = Report.objects.all()
+    return render(request, 'choicemaster/report.html', context)
+
