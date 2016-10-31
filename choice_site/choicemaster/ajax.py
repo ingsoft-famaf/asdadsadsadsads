3a8c71f50f77d46fa6b2aa2746f646182b00b8d9
Agustin Urquiza <agustin.h.urquiza@gmail.com>
2016-10-31 15:26:57 -0300

    Lo que no estaba

--- choice_site/choicemaster/ajax.py
+++ choice_site/choicemaster/ajax.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from models import Report, Question
+from models import Report, Question, Answer
+
 
 @csrf_exempt
 def delete_report(request):
@@ -15,9 +16,51 @@ def delete_report(request):
 @csrf_exempt
 def delete_question(request):
     if request.is_ajax() and request.POST:
+        question = Question.objects.get(id=request.POST.get('idQ'))
+        question.delete()
+        return HttpResponse("Delted")
+    else:
+        return HttpResponse("No deleted")
+
+@csrf_exempt
+def delete_answer(request):
+    if request.is_ajax() and request.POST:
         report = Report.objects.get(id=request.POST.get('idR'))
         report.report_state = Report.EVALUATED
         report.save()
 
-        question = Question.objects.get(id=request.POST.get('idQ'))
-        question.delete()
+        Answer.objects.get(id=request.POST.get('idA')).delete()
+        return HttpResponse("Delted")
+    else:
+        return HttpResponse("No deleted")
+
+@csrf_exempt
+def edit_question(request):
+    if request.is_ajax() and request.POST:
+        question = Question.objects.get(id=request.POST.get('id'))
+        new_value =  request.POST.get('newValue')
+        question.question_text = new_value;
+        question.save()
+
+        report = Report.objects.get(id=request.POST.get('idR'))
+        report.report_state = Report.EVALUATED
+        report.save()
+        return HttpResponse("Delted")
+    else:
+        return  HttpResponse("No deleted")
+
+
+@csrf_exempt
+def edit_ans(request):
+    if request.is_ajax() and request.POST:
+        ans = Answer.objects.get(id=request.POST.get('id'))
+        new_value =  request.POST.get('newValue')
+        ans.answer_text = new_value;
+        ans.save()
+
+        report = Report.objects.get(id=request.POST.get('idR'))
+        report.report_state = Report.EVALUATED
+        report.save()
+        return HttpResponse("Delted")
+    else:
+        return  HttpResponse("No deleted")
--- choice_site/choicemaster/urls.py
+++ choice_site/choicemaster/urls.py
@@ -12,6 +12,10 @@ urlpatterns = [
         name='add_question_w_sdeect_topic'),
     url(r'^report/$', views.report, name='report'),
     url(r'^ajax/delete/report$', ajax.delete_report, name="delete_report"),
-    url(r'^ajax/delete/question$', ajax.delete_question, name="delete_question")
+    url(r'^ajax/delete/question/$', ajax.delete_question,
+        name="delete_question"),
+    url(r'^ajax/delete/answer/$', ajax.delete_answer, name="delete_answer"),
+    url(r'^ajax/edit/question/$', ajax.edit_question, name="Edit Question"),
+    url(r'^ajax/edit/answer/$', ajax.edit_ans, name="Edit Answer")
 
 ]
