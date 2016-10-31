092691a17b98e1cc2911c477397ca88b813f6640
Ignacio Tiraboschi <ignatirabo@gmail.com>
2016-10-31 00:41:40 -0300

    Borradas vistas innecesarias debido a la nueva implementación de add_question

--- choice_site/choicemaster/urls.py
+++ choice_site/choicemaster/urls.py
@@ -4,12 +4,6 @@ from . import views, ajax
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'ajax_view/$', ajax.ajax_view, name='ajax_view'),
-
     url(r'^add/question/$', views.add_question, name='add_question'),
-    url(r'^add/question/(?P<subject_id>[0-9]+)/$',views.add_question_w_subject
-        , name='add_question_w_subject'),
-    url(r'^add/question/(?P<subject_id>[0-9]+)/(?P<topic_id>[0-9]+)/$',
-        views.add_question_w_subject_topic,
-        name='add_question_w_subject_topic'),
     url(r'^report/$', views.report, name='report')
 ]
